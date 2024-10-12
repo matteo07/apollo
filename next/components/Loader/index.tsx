325e16819e9909e66b2e553a32e5aa6d0edd5ddf
matteofranceschidemarchi <matteodema17@gmail.com>
2024-10-12 15:01:52 +0200

    fix: added alt to next image

--- next/components/Loader/index.tsx
+++ next/components/Loader/index.tsx
@@ -3,6 +3,6 @@ import { wrapper } from '@components/Loader/styles'
 
 export const Loader = () => (
     <div style={wrapper}>
-        <Image src='/wind.gif' width={120} height={75} />
+        <Image src='/wind.gif' width={120} height={75} alt='loader-spinner' />
     </div>
 )
